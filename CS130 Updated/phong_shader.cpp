694c31bd16299da3806ca6e4c357ddc4c9225cd5
awadhamza <34406043+awadhamza@users.noreply.github.com>
2019-01-27 08:54:28 -0800

    Pushing untested phong_shader.cpp

--- CS130 Updated/phong_shader.cpp
+++ CS130 Updated/phong_shader.cpp
@@ -10,5 +10,39 @@ Shade_Surface(const Ray& ray,const vec3& intersection_point,
 {
     vec3 color;
     TODO; //determine the color
+
+    //Add ambient component
+    vec3 Iambient = world.ambient_intensity * world.ambient_color * this.color_ambient;
+
+    for(unsigned i = 0; i < lights.size(); i++){
+        
+        Ray shadow_ray(intersection_point, (lights.at(i)->position - intersection_point));   // shadow_ray points from intersection point towards light
+
+        double maxDistance = (lights.at(i)->position - intersection_point)/(shadow_ray.direction)    // max Ray's intersection distance, t
+
+        
+        Hit tempHit = Closest_Intersection(shadow_ray); // Finding closest intersectionf or shadow_ray
+
+
+        if(tempHit.object != NULL && (tempHit.dist != 0 || tempHit.dist > maxDistance){    // If shadow_ray doesn't intersect any object between initial point and light
+            //Compute diffuse and specular
+            vec3 Idiffuse = Emitted_Light(shadow_ray) * max(dot(normal, shadow_ray.normalized()), 0);
+            
+
+            vec3 v = -ray.direction;
+            vec3 r = v - 2 * dot(v, normal) * normal;
+
+            vec3 Ispecular = Emitted_Light(shadow_ray) * max(dot(v, r), 0); 
+
+            color += Idiffuse + Ispecular;
+            
+        }
+
+        
+
+    }
+    
+    color += Iambient;
+
     return color;
 }
